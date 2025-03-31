4cd77a310b4b54c081138075ae523f60f5c7a541
Amarnath Bagineni <amarnath@greyquill.io>
2025-03-31 09:16:30 +0530

    Updating website content

--- websire_react/src/components/Navigation.jsx
+++ websire_react/src/components/Navigation.jsx
@@ -8,7 +8,7 @@ function Navigation() {
       <ul className="flex justify-center space-x-8">
         {links.map((link, index) => (
           <li key={index}>
-            <button className="text-gray-600 hover:text-gray-900 hover:font-semibold">{link}</button>
+            <button className="text-blue-500 font-bold hover:text-blue-600 hover:underline">{link}</button>
           </li>
         ))}
       </ul>
